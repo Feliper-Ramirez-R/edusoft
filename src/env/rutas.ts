4281b19ba2246a956568ec51835eb87ad48b8e7f
Felipe Ramirez <Framirez@Tacmo.local>
2023-10-16 11:38:14 -0500

    footer y navbar

--- src/env/rutas.ts
+++ src/env/rutas.ts
@@ -2,9 +2,13 @@
 
 const rutas = {
    
-    /* Desarrollo */
+    /* produccion */
     ruta:  'https://dev-edusoft-server.up.railway.app/api/',
-     doc:  'https://dev-edusoft-server.up.railway.app/docs/'
+   
+   
+    /* Desarrollo */
+    // ruta:  'https://edusoft-server-development.up.railway.app/api/',
+   
 }
 
 export {rutas}
