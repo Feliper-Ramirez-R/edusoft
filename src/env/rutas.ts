54b3f4eb68aa4aa5c8ac2953d914bf818dede405
Felipe Ramirez <Framirez@Tacmo.local>
2024-01-23 19:10:57 -0500

    despliegue produccion

--- src/env/rutas.ts
+++ src/env/rutas.ts
@@ -3,11 +3,11 @@
 const rutas = {
    
     /* produccion */
-    // ruta:  'https://dev-edusoft-server.up.railway.app/api/',
+    ruta:  'https://dev-edusoft-server.up.railway.app/api/',
    
    
     /* Desarrollo */
-     ruta:  'https://edusoft-server-development.up.railway.app/api/',
+    //  ruta:  'https://edusoft-server-development.up.railway.app/api/',
 
     /* Local */
         // ruta:  'http://192.168.0.86:3500/api/',
